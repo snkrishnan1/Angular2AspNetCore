2d56599f81c9d2a033894ed529c3fd3522fb0520
NavaneethaKrishnan Sampath <snkrishnan1@yahoo.co.in>
2016-09-21 12:14:29 -0400

    code changes

--- Firebolt.Web/wwwroot/app/AppComponent.js
+++ Firebolt.Web/wwwroot/app/AppComponent.js
@@ -14,7 +14,7 @@ let AppComponent = class AppComponent {
 AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
-        template: '<h1>My First Angular 2 App</h1>'
+        template: '<h1>My First Angular 2 App 123</h1>'
     }), 
     __metadata('design:paramtypes', [])
 ], AppComponent);
